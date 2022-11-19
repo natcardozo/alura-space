735b447dedbc4f0feb6d028612b9769ff6d3c574
Nathalia Cardozo <nathaliacardozosilva@gmail.com>
2022-11-18 22:41:32 -0300

    first commit

---
+++ src/paginas/PaginaInicial/index.jsx
@@ -0,0 +1,23 @@
+import Cabecalho from "../../componentes/Cabecalho";
+import Menu from "../../componentes/Menu";
+
+import banner from "./banner.png";
+
+import styles from "./PaginaInicial.module.scss";
+
+export default function PaginaInicial() {
+    return (
+        <>
+            <Cabecalho />
+            <main>
+                <section className={styles.principal}>
+                    <Menu />
+                    <div className={styles.principal__imagem}>
+                        <h1>A galeria mais completa do espaço</h1>
+                        <img src={banner} alt="O Planeta Terra vista do espaço" />
+                    </div>
+                </section>
+            </main>
+        </>
+    )
+}
