471e8e5dc7db850409a96d82bc0eb1a02a3ba45b
Nathalia Cardozo <nathaliacardozosilva@gmail.com>
2022-11-20 16:34:18 -0300

    filtro

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -1,17 +1,26 @@
-import Tags from "../Tags";
-
+import { useState } from "react";
 import styles from "./Galeria.module.scss";
+
+import Tags from "../Tags";
 
 import fotos from "./fotos.json";
 import Cards from "./Cards";
 
 export default function Galeria() {
+  const [itens, setItens] = useState(fotos);
+  const tags = [...new Set(fotos.map((foto) => foto.tag))]
+
+  const filtrarFotos = (tag) => {
+    const fotosFiltradas = fotos.filter((item) => item.tag === tag)
+    setItens(fotosFiltradas)
+  }
+
   return (
     <section className={styles.galeria}>
         <h2>Navegue pela galeria</h2>
-        <Tags />
+        <Tags tags={tags} filtrarFotos={filtrarFotos} setItens={setItens} />
         <ul className={styles.galeria__cards}>
-          {fotos.map((foto) => <Cards foto={foto} />)}
+          {itens.map((foto) => <Cards key={foto.id} foto={foto} />)}
         </ul>
     </section>
   )
--- src/componentes/Tags/index.jsx
+++ src/componentes/Tags/index.jsx
@@ -1,15 +1,14 @@
 import styles from "./Tags.module.scss";
+import fotos from "../Galeria/fotos.json";
 
-export default function Tags() {
+export default function Tags({ tags, filtrarFotos, setItens }) {
   return (
     <div className={styles.tags}>
-        <p>Filtre por tags:</p>
-        <ul className={styles.tags__lista}>
-            <li>Estrelas</li>
-            <li>Galáxias</li>
-            <li>Lua</li>
-            <li>Planetas</li>
-        </ul>
+      <p>Filtre por tags:</p>
+      <ul className={styles.tags__lista}>
+        {tags.map((tag) => <li onClick={() => filtrarFotos(tag)} key={tag}>{tag}</li>)}
+        <li onClick={() => setItens(fotos)}>Todas</li>
+      </ul>
     </div>
   )
 }
